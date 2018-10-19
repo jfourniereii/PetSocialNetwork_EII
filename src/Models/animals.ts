c8030c6fae3f578ee6f658c380425b569274572f
Josh <j.fournier@sciencesu-lyon.fr>
2018-10-19 17:02:54 +0200

    Page Menu

---
+++ src/Models/animals.ts
@@ -0,0 +1,15 @@
+export class animals{
+    
+    namePet : string
+    typePet : string
+    agePet : number
+    //firstNameOwnerPet : string
+    //lastNameOwnerPet : string
+
+    constructor (namePet, typePet, agePet){
+
+    }
+
+}
+
+
